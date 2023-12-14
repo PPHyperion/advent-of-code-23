fe36ea7043be59252bfef4f16aeb67b6f62323df
PPHyperion <paul.pulter@gmail.com>
2023-12-14 14:12:34 +0000

    day 01 part 1: refactor

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,39 +1,38 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut sum = 0;
-
-    for line in input.lines() {
-        let mut digi1: (usize, char) = (std::usize::MAX, '0');
-        let mut digi2: (usize, char) = (std::usize::MIN, '0');
-
-        for (i, c) in line.char_indices() {
-            if !c.is_digit(10) {
-                continue;
-            };
-
-            if i <= digi1.0 {
-                digi1 = (i, c)
-            }
-            if i >= digi2.0 {
-                digi2 = (i, c)
-            }
-        }
-
-        let number: u32 = [digi1.1, digi2.1]
-            .iter()
-            .collect::<String>()
-            .parse::<u32>()
-            .unwrap();
-
-        sum += number;
-    }
-
-    Some(sum)
+    let result = input.lines().map(map_part_one).sum();
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
+}
+
+fn map_part_one(line: &str) -> u32 {
+    let mut digi1: (usize, char) = (std::usize::MAX, '0');
+    let mut digi2: (usize, char) = (std::usize::MIN, '0');
+
+    for (i, c) in line.char_indices() {
+        if !c.is_digit(10) {
+            continue;
+        };
+
+        if i <= digi1.0 {
+            digi1 = (i, c)
+        }
+        if i >= digi2.0 {
+            digi2 = (i, c)
+        }
+    }
+
+    let number: u32 = [digi1.1, digi2.1]
+        .iter()
+        .collect::<String>()
+        .parse::<u32>()
+        .unwrap();
+
+    number
 }
 
 #[cfg(test)]
