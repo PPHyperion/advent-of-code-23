2c2465a0c38c7fa510b5104fff1502734d27b03b
PPHyperion <paul.pulter@gmail.com>
2023-12-14 13:51:06 +0000

    day 01 part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,54 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let mut digi1: (usize, char) = (std::usize::MAX, '0');
+        let mut digi2: (usize, char) = (std::usize::MIN, '0');
+
+        for (i, c) in line.char_indices() {
+            if !c.is_digit(10) {
+                continue;
+            };
+
+            if i <= digi1.0 {
+                digi1 = (i, c)
+            }
+            if i >= digi2.0 {
+                digi2 = (i, c)
+            }
+        }
+
+        let number: u32 = [digi1.1, digi2.1]
+            .iter()
+            .collect::<String>()
+            .parse::<u32>()
+            .unwrap();
+
+        sum += number;
+    }
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
